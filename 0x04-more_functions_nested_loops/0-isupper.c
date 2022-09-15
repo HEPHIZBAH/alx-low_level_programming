d001d3cab20e6074a07cd80de216bf44232229fb
HEPHIZBAH <joyflourish2022@gmail.com>
2022-09-15 04:59:05 -0700

    first attempts

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* main - check the code.
+*
+* Return: Always 0.
+*/
+
+int main(void)
+{
+  char c = 'a';
+if (c == _isupper(c))
+{
+_putchar("%c: %d\n", c, _isupper(c));
+return (1);
+}
+ else (c !== _isupper(c))
+{
+_putchar("%c: %d\n", c, _isupper(c));  
+return (0);
+}
+}
