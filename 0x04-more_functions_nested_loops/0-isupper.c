663a955c1725d29185b867f3c75f343423ee0712
HEPHIZBAH <joyflourish2022@gmail.com>
2022-09-15 05:42:51 -0700

    fix

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,17 +6,14 @@
 * Return: Always 0.
 */
 
-int main(void)
+void main(void)
 {
-char c;
 if (c >= 65 && c <= 90)
 {
-_putchar("%c: %d\n", c, _isupper(c));
 return (1);
 }
 else
 {
-_putchar("%c: %d\n", c, _isupper(c));
 return (0);
 }
 }
