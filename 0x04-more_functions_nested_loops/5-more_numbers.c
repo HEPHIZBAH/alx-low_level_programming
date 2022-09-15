606414aa0424c85070aaf5ce0dde20936372b83a
HEPHIZBAH <joyflourish2022@gmail.com>
2022-09-15 08:07:45 -0700

    fix

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,11 +8,13 @@
 void more_numbers(void)
 {
 int sh, bh;
-for (sh = 0; sh <= 10; sh++)
+for (sh = 0; sh < 10; sh++)
 {
 for (bh = 0; bh < 15; bh++)
 {
-_putchar(bh++);
+if (bh >= 10)
+_putchar((bh / 10) + 48);
+_putchar((bh % 10) + 48);
 }
 _putchar(\n);
 }
