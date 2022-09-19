8590203ebf2455084a61cffcc17b04c7595e2fd4
HEPHIZBAH <joyflourish2022@gmail.com>
2022-09-19 14:07:26 -0700

    try

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/*
+ * strlen - gets the length of a string
+ * return - returns a value
+ */
+
+int _strlen(char *s);
+{
+	char s[5] = "hello";
+	int b = strlen(s);
+	return(b);
+}
+
